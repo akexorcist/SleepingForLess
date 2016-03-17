eff0868fad3c8251f541910ac9ff4177d0e5096d
Akexorcist <akexorcist@gmail.com>
2016-03-17 20:56:18 +0700

    Add download for image preview

--- app/src/main/java/com/akexorcist/sleepingforless/view/post/ImagePostPreviewActivity.java
+++ app/src/main/java/com/akexorcist/sleepingforless/view/post/ImagePostPreviewActivity.java
@@ -1,9 +1,15 @@
 package com.akexorcist.sleepingforless.view.post;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 
 import com.akexorcist.sleepingforless.R;
@@ -15,26 +21,49 @@ import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+import com.flipboard.bottomsheet.BottomSheetLayout;
+import com.flipboard.bottomsheet.commons.IntentPickerSheetView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by Akexorcist on 3/13/2016 AD.
  */
 public class ImagePostPreviewActivity extends SFLActivity implements View.OnClickListener {
     private SubsamplingScaleImageView ivPreview;
-    private FloatingActionButton fabPreviewClose;
+    private FloatingActionButton fabClose;
+    private FloatingActionButton fabDownload;
     private String fullUrl;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_image_preview);
-        ivPreview = (SubsamplingScaleImageView) findViewById(R.id.iv_preview);
-        fabPreviewClose = (FloatingActionButton) findViewById(R.id.fab_preview_close);
-
-        fabPreviewClose.setOnClickListener(this);
 
         getBundleFromIntent();
+        bindView();
+        setupView();
+        downloadImageToPreview();
+    }
 
+    private void bindView() {
+        ivPreview = (SubsamplingScaleImageView) findViewById(R.id.iv_preview);
+        fabClose = (FloatingActionButton) findViewById(R.id.fab_preview_close);
+        fabDownload = (FloatingActionButton) findViewById(R.id.fab_preview_download);
+    }
+
+    private void setupView() {
+        fabClose.setOnClickListener(this);
+        fabDownload.setOnClickListener(this);
+    }
+
+    private void downloadImageToPreview() {
         Glide.with(this)
                 .load(fullUrl)
                 .asBitmap()
@@ -54,8 +83,66 @@ public class ImagePostPreviewActivity extends SFLActivity implements View.OnClic
 
     @Override
     public void onClick(View v) {
-        if (v == fabPreviewClose) {
+        if (v == fabClose) {
             finish();
+        } else if (v == fabDownload) {
+            downloadImage();
         }
+    }
+
+    private void downloadImage() {
+        download(fullUrl, new SimpleTarget<File>() {
+            @Override
+            public void onResourceReady(File file, GlideAnimation<? super File> glideAnimation) {
+                File destinationFile = new File(getDownloadsDirectory(), file.getName().replaceAll(".", "_") + ".jpg");
+                copyToDownloadsDirectory(file, destinationFile);
+                updateImageToMediaScanner(destinationFile);
+                showSavedMessage("Saved image to Downloads directory.", destinationFile);
+            }
+        });
+    }
+
+    public void updateImageToMediaScanner(File file) {
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        intent.setData(Uri.fromFile(file));
+        sendBroadcast(intent);
+    }
+
+    private void download(String url, SimpleTarget<File> simpleTarget) {
+        Glide.with(this)
+                .load(url)
+                .downloadOnly(simpleTarget);
+    }
+
+    private File getDownloadsDirectory() {
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+    }
+
+    private void copyToDownloadsDirectory(File source, File destination) {
+        try {
+            InputStream inputStream = new FileInputStream(source);
+            OutputStream outputStream = new FileOutputStream(destination);
+
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buf)) > 0) {
+                outputStream.write(buf, 0, len);
+            }
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+        }
+    }
+
+    private void showSavedMessage(String message, final File file) {
+        Snackbar.make(ivPreview, message, Snackbar.LENGTH_LONG).setAction("View", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.fromFile(file), "image/*");
+                startActivity(intent);
+            }
+        }).show();
     }
 }
