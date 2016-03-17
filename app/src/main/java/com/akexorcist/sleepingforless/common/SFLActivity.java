c7f06aad970debd9578bc9166e95cf2fce890bdc
Akexorcist <akexorcist@gmail.com>
2016-03-18 00:50:11 +0700

    Add activity transition

--- app/src/main/java/com/akexorcist/sleepingforless/common/SFLActivity.java
+++ app/src/main/java/com/akexorcist/sleepingforless/common/SFLActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.akexorcist.sleepingforless.R;
 import com.akexorcist.sleepingforless.bus.BusProvider;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -15,6 +16,12 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
  * Created by Akexorcist on 3/10/2016 AD.
  */
 public class SFLActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.anim_simple_fade_in, R.anim.anim_simple_fade_out);
+    }
 
     protected void openActivity(Class<? extends Activity> activityClass) {
         startActivity(new Intent(this, activityClass));
