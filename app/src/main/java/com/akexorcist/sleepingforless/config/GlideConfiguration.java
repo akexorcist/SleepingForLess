2a61c700edfe31321af38f8e2ec95b75ed133e5c
Akexorcist <akexorcist@gmail.com>
2016-03-21 17:13:22 +0700

    Reduce Glide Configuration Resource

--- app/src/main/java/com/akexorcist/sleepingforless/config/GlideConfiguration.java
+++ app/src/main/java/com/akexorcist/sleepingforless/config/GlideConfiguration.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.module.GlideModule;
 
 /**
@@ -14,7 +16,12 @@ public class GlideConfiguration implements GlideModule {
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
         // Apply options to the builder here.
+        int cacheSize = 1024 * 1024 * 20;
+        int memorySize = 1024 * 1024 * 20;
+        String cacheDirectory = context.getCacheDir().getAbsolutePath();
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, cacheDirectory, cacheSize));
         builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
+        builder.setMemoryCache(new LruResourceCache(memorySize));
     }
 
     @Override
