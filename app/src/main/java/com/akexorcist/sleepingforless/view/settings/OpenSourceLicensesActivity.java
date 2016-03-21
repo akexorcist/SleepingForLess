4ed0c79d03d9ed483d7d1320159701f9b8621ce6
Akexorcist <akexorcist@gmail.com>
2016-03-22 01:20:03 +0700

    Add open source licenses Add preference summary

---
+++ app/src/main/java/com/akexorcist/sleepingforless/view/settings/OpenSourceLicensesActivity.java
@@ -0,0 +1,15 @@
+package com.akexorcist.sleepingforless.view.settings;
+
+import android.os.Bundle;
+
+import com.akexorcist.sleepingforless.R;
+import com.akexorcist.sleepingforless.common.SFLActivity;
+
+public class OpenSourceLicensesActivity extends SFLActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_source_licenses);
+    }
+}
