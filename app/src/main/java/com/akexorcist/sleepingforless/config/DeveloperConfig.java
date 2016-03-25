d7840ac1987e2c226c14f4dde11799ffe513d923
Akexorcist <akexorcist@gmail.com>
2016-03-25 20:08:11 +0700

    Enable Google Analytics

--- app/src/main/java/com/akexorcist/sleepingforless/config/DeveloperConfig.java
+++ app/src/main/java/com/akexorcist/sleepingforless/config/DeveloperConfig.java
@@ -4,5 +4,5 @@ package com.akexorcist.sleepingforless.config;
  * Created by Akexorcist on 3/24/2016 AD.
  */
 public class DeveloperConfig {
-    public static final boolean ALLOW_ANALYTICS = false;
+    public static final boolean ALLOW_ANALYTICS = true;
 }
