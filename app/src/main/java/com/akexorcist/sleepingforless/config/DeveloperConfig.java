e49172046106dde50ebc52246cef9b094f33093c
Akexorcist <akexorcist@gmail.com>
2016-03-27 13:35:10 +0700

    Config for release

--- app/src/main/java/com/akexorcist/sleepingforless/config/DeveloperConfig.java
+++ app/src/main/java/com/akexorcist/sleepingforless/config/DeveloperConfig.java
@@ -4,7 +4,7 @@ package com.akexorcist.sleepingforless.config;
  * Created by Akexorcist on 3/24/2016 AD.
  */
 public class DeveloperConfig {
-    public static final boolean ALLOW_ANALYTICS = false;
-    public static final boolean ALLOW_CRASH_ACTIVITY = false;
+    public static final boolean ALLOW_ANALYTICS = true;
+    public static final boolean ALLOW_CRASH_ACTIVITY = true;
     public static final boolean ALLOW_DEBUG_CONTENT = true;
 }
