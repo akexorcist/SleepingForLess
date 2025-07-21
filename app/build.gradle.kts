454f14ae4dadc013ee8fea1e5f46bb4914c34850
Akexorcist <akexorcist@gmail.com>
2025-07-22 03:52:14 +0700

    chore(deps): Update dependencies to latest versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,14 +7,14 @@ plugins {
 
 android {
     namespace = "com.akexorcist.sleepingforless"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.akexorcist.sleepingforless"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 212
-        versionName = "2.2.1"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
